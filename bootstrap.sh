17569e350fd0c6848c437128707acc33732bfa1d
bg2slc <92640148+bg2slc@users.noreply.github.com>
2022-07-15 00:19:43 -0400

    script
    simple script, probably too simple but it's for virtual machines

---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Intended for use AFTER disk partitioning, assuming:
+#/dev/vda1 is /boot
+#/dev/vda2 is /
+#/dev/vda3 is swap
+
+timedatectl set-ntp true
+
+mkfs.ext4 /dev/vda1
+mkfs.ext4 /dev/vda2
+mkswap /dev/vda3
+
+mount /dev/vda2 /mnt
+mkdir /mnt/boot
+mount /dev/vda1 /mnt/boot
+swapon /dev/vda3
+Replying to @DusK_The_Stray: How late am I? just got here
+pacstrap /mnt base linux linux-firmware vi neovim awesome lightdm netctl grub
+
+arch-chroot /mnt
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+ln -sf /usr/share/zoneinfo/Amercia/Toronto /etc/localtime
+hwclock --systohc
+
+nvim /etc/locale.gen
+
+locale-gen
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "daedalus" > /etc/hostname
+
+grub-install --target=i386-pc /dev/vda1
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "remember to create user useradd -m -G wheel USERNAME"
